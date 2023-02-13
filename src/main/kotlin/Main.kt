1530e479634d8df2f97bae3107cc3a205fde4ba9
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-02-13 15:16:12 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    fullName("Marrion")
+var x=23 % 2
+    println(x)
+    var c = 42+20+30+8
+    println(c)
+    interestingfact("creative")
+}
+fun fullName (name: String){
+    println("Hello $name")
+}
+fun modulus(num1:Int, num2:Int):Int{
+    var modulus= num1 % num2
+    return modulus
+
+
+}
+fun sum(num1: Int,num2: Int,num3:Int,num4:Int):Int{
+    var sum =num1+num2+num3+num4
+    return sum
+
+}
+fun interestingfact(yourself:String){
+    println(yourself)
+
+}
